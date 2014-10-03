f40a4d96b72f0b7f28041d284e1968e18984e8d5
yejian <yejian122@qq.com>
2014-10-03 15:24:59 +0800

    [mod] user-extention.js

--- user-extention.js
+++ user-extention.js
@@ -1,14 +1,4 @@
 Selenium.prototype.doStoreRandom = function(variableName){
-// random = Math.floor(Math.random()*10);
-var random = 0;
-while (true) {
-	random = Math.floor(Math.random()*10);
-	if (random >5) {
-		continue;
-	}else {
-		break;
-	}
-}
-
+random = Math.floor(Math.random()*100000);
 storedVars[variableName] = random;
 }
