8a6b8d332fc97e9f43fb883f3f1410efed9753f3
Allen Koo <crossgate9@gmail.com>
2014-10-09 16:56:16 +0800

    [mod] add info method

--- user-extension.js
+++ user-extension.js
@@ -3,15 +3,9 @@ Selenium.prototype.doStoreRandom = function(_varname, _range){
   storedVars[_varname] = random;
 }
 
-// var i = 0;
-
-// Selenium.prototype.doAddInfo = function(_varname, _address) {
-// 	var arr = new Array();
-// 	arr[i] = _address;
-// 	i += 1;
-// 	var result = '';
-// 	for (j=0; j<array.lenth; j++) {
-// 		result += array[j] + '\n';
-// 	}
-// 	storedVars[_varname] = result;
-// }
+Selenium.prototype.doAddInfo = function(_varname, _info) {
+  if (typeof storedVars[_varname] === 'undefined') {
+    storedVars[_varname] = [];
+  }
+	storedVars[_varname].push(_info);
+}
